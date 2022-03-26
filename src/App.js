40d8f173896904e91c062b674270ad95d35be7ea
Anis <anis01000001@gmail.com>
2022-03-26 15:47:13 +0100

    refactor loading fonts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 import { observer } from 'mobx-react';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StatusBar } from 'react-native';
 import AppScreen from './Screens/AppScreen';
 import LoginScreen from './Screens/LoginScreen';
 import connectionStore from './stores/conncetionStore';
 
-function App () {
-  async function loadFonts() {
-    await Font.loadAsync({
-      "Poppins-Bold": require('./assets/fonts/Poppins-Bold.ttf'),
-      "Poppins-Italic": require('./assets/fonts/Poppins-Italic.ttf'),
-      "Poppins-Medium": require('./assets/fonts/Poppins-Medium.ttf'),
-      "Poppins-Regular": require('./assets/fonts/Poppins-Regular.ttf'),
-      "Poppins-SemiBold": require('./assets/fonts/Poppins-SemiBold.ttf'),
-    });
-  }
-
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    loadFonts().then(() => setFontsLoaded(true));
-  }, []);
+function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Italic': require('./assets/fonts/Poppins-Italic.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+  });
 
   return (
     <>
