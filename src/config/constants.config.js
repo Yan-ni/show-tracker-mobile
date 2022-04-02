07eb4592e8d6fdf1a2c37d4f39e6e93d21a217ef
Anis <anis01000001@gmail.com>
2022-04-02 02:56:32 +0100

    add development environement variable

--- src/config/constants.config.js
+++ src/config/constants.config.js
@@ -1,4 +1,1 @@
-// never push this varibla as true
-const isDevelopment = false;
-
-export const BASE_URL = isDevelopment ? 'http://localhost:5000': 'https://show-tracker-server.herokuapp.com';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000': 'https://show-tracker-server.herokuapp.com';
